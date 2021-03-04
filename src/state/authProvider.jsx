935304d21fcef235ada14eaa251b9483436dd216
Jumpybuns <ethanpiercepresents@gmail.com>
2021-03-04 12:14:31 -0800

    tighten up token transfer

--- src/state/authProvider.jsx
+++ src/state/authProvider.jsx
@@ -10,23 +10,20 @@ export const AuthProvider = ({ children }) => {
     const params = new URLSearchParams(window.location.search);
     const code = params.get('code');
     // eslint-disable-next-line max-len
-    if(!accessToken && !code) window.location =  'https://app.alpaca.markets/oauth/authorize?response_type=code&client_id=edb6dea4ea1d646cd3bb3f82667f33df&redirect_uri=http://localhost:7891&scope=%20trading';
-    
-    fetch('https://localhost:7890/api/v1/auth/token',  {
+    if (!accessToken && !code)
+      window.location =
+        'https://app.alpaca.markets/oauth/authorize?response_type=code&client_id=edb6dea4ea1d646cd3bb3f82667f33df&redirect_uri=http://localhost:7891&scope=%20trading';
+
+    fetch('http://localhost:7890/api/v1/auth/token', {
       method: 'Post',
       headers: { 'content-type': 'application/json' },
-      body: JSON.stringify({ code })
-
+      body: JSON.stringify({ code }),
     })
-      .then(res => res.json())
+      .then((res) => res.json())
       .then(({ accessToken }) => setAccessToken(accessToken));
-
   }, []);
 
   return (
-    <AuthContext.Provider value={accessToken}>
-      {children}
-    </AuthContext.Provider>
+    <AuthContext.Provider value={accessToken}>{children}</AuthContext.Provider>
   );
 };
-
