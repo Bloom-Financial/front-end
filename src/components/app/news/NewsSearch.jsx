0e27166dbb413616c0b87376d46a98ed7dbd40c4
ryanleviathan <ryan.m.carreras@gmail.com>
2021-03-05 11:17:48 -0800

    Artcile

--- src/components/app/news/NewsSearch.jsx
+++ src/components/app/news/NewsSearch.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styles from './Search.css';
 
 const Search = ({ search, onChange }) => (
   <input
-    className={styles.Search}
     type="text"
     placeholder="Search here..."
     value={search}
