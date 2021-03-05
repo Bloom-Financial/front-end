f4c2a16dd1ddf883ee606133f51810dfe2720cb9
ryanleviathan <ryan.m.carreras@gmail.com>
2021-03-05 11:13:19 -0800

    news service

---
+++ src/services/newsApi.js
@@ -0,0 +1,18 @@
+const process = require('dotenv');
+
+export const findArticles = (search) => {
+  return fetch(
+    `https://newsapi.org/v2/everything?q=${search}&
+    language=en&
+    pageSize=50&
+    apiKey=${process.env.NEWS_API_KEY}`.replace(/\s/g, '')
+  )
+    .then((res) => res.json())
+    .then(({ articles }) =>
+      articles.map((article) => ({
+        title: article.title,
+        url: article.url,
+        image: article.urlToImage,
+      }))
+    );
+};
