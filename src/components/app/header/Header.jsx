464cd669f72375f230925fea73c1481a5ae4223e
sarah-svg <kjskillen@gmail.com>
2021-03-05 13:06:22 -0800

    header component

--- src/components/app/header/Header.jsx
+++ src/components/app/header/Header.jsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { setState } from 'react';
 import PropTypes from 'prop-types';
 
-function Header(props) {
+const Header = ({ username, profile, onSubmit }) => {
+  // const [username, setUsername ] = setState('');
+  // const [profile, setProfile ] = setState('');
+
   return (
-    <div>
-            
-    </div>
+    <>
+      <form>
+        <input id="username" 
+          type="text" 
+          value={username}>username: {username} </input>
+        <input id="profile"
+          type="text"
+          value={profile}>profile:{profile}</input>
+        <button onSubmit={onSubmit}>Update Profile</button>   
+      </form>
+      <button onSubmit={onSubmit}>login out</button>
+    </>
   );
-}
+};
 
 Header.propTypes = {
-
+  username: PropTypes.string.isRequired,
+  profile: PropTypes.string.isRequired,
+  onSubmit: PropTypes.func.isRequired
 };
 
 export default Header;
