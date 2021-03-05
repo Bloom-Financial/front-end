a44cee32864ac16a562ecff13beb357ac852af69
Jumpybuns <ethanpiercepresents@gmail.com>
2021-03-05 11:28:30 -0800

    UserPaperPostions complete but not rendered anywhere

--- src/containers/LoginPage.jsx
+++ src/containers/LoginPage.jsx
@@ -3,12 +3,9 @@ import React from 'react';
 import { useAlpacaRedirect } from '../state/authProvider';
 import styles from './LoginPage.css';
 
-
-
 export default function LoginPage() {
   const redirectToAlpaca = useAlpacaRedirect();
 
- 
   return (
     <div className={styles.log}>
       <button onClick={redirectToAlpaca}>C o n t i n u e</button>
