5f51695f1afe11cc62e59cac440fc28ee211256d
sarah-svg <kjskillen@gmail.com>
2021-03-08 15:52:13 -0800

    grab acces token

--- src/state/authProvider.jsx
+++ src/state/authProvider.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
 const AuthContext = React.createContext(null);
@@ -7,26 +7,32 @@ const AuthContext = React.createContext(null);
 export const AuthProvider = ({ children }) => {
   const [accessToken, setAccessToken] = useState();
 
-  const redirectToAlpaca = () => {
+  useEffect(() => {
     const params = new URLSearchParams(window.location.search);
     const code = params.get('code');
+    if(code){
+      fetch('http://localhost:7890/api/v1/auth/token', {
+        method: 'Post',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify({ code }),
+      })
+        .then((res) => res.json())
+        .then(({ accessToken }) => {
+          setAccessToken(accessToken);
+          console.log(accessToken, 'accesstoken');
+        });
+    }
+
+  }, []);
+
+
+  const redirectToAlpaca = () => {
+ 
     // eslint-disable-next-line max-len
     // if (!accessToken && !code) {
     window.location =
         'https://app.alpaca.markets/oauth/authorize?response_type=code&client_id=edb6dea4ea1d646cd3bb3f82667f33df&redirect_uri=http://localhost:7891/home&scope=account:write%20trading';
 
-    fetch('http://localhost:7890/api/v1/auth/token', {
-      method: 'Get',
-      headers: { 'content-type': 'application/json' },
-      body: JSON.stringify({ code }),
-    })
-      .then((res) => res.json())
-      .then(console.log(json, accessToken))
-      .then(({ accessToken }) => {
-        setAccessToken(accessToken);
-        //  console.log(acessToken 'acesstoken');
-      });
-    // } else useHistory().push('/home');
   };
   return (
     <AuthContext.Provider value={{ accessToken, redirectToAlpaca }}>
