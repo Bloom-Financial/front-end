9d3819a52b376d0e9576d0d46484beda303e1da5
sarah-svg <kjskillen@gmail.com>
2021-03-08 15:27:10 -0800

    saved acess token in state

--- src/services/alpacaGetUserPortfolioAPI.js
+++ src/services/alpacaGetUserPortfolioAPI.js
@@ -1,9 +1,8 @@
-export const getUserPaperPositions = () => {
+export const getUserPaperPositions = (accessToken) => {
   return fetch('https://paper-api.alpaca.markets/v2/positions', {
     method: 'GET',
     headers: {
-      'APCA-API-KEY-ID': 'PKCMVVTLMUA016H9WEG5',
-      'APCA-API-SECRET-KEY': 'CQVgOScXGPdpDjeoFR9z79PuHNRur5B4gAL4Bjmr',
+      'Authorization': `Bearer ${accessToken}`
     },
   })
     .then((res) => res.json())
@@ -25,3 +24,5 @@ export const getAssetsByStockName = (stock) => {
       console.error(err);
     });
 };
+  //  'APCA-API-KEY-ID': 'PKCMVVTLMUA016H9WEG5',
+  //     'APCA-API-SECRET-KEY': 'CQVgOScXGPdpDjeoFR9z79PuHNRur5B4gAL4Bjmr',
--- src/state/authProvider.jsx
+++ src/state/authProvider.jsx
@@ -11,20 +11,22 @@ export const AuthProvider = ({ children }) => {
     const params = new URLSearchParams(window.location.search);
     const code = params.get('code');
     // eslint-disable-next-line max-len
-    if (!accessToken && !code) {
-      window.location =
-        'https://app.alpaca.markets/oauth/authorize?response_type=code&client_id=edb6dea4ea1d646cd3bb3f82667f33df&redirect_uri=http://localhost:7891/home&scope=%20trading';
+    // if (!accessToken && !code) {
+    window.location =
+        'https://app.alpaca.markets/oauth/authorize?response_type=code&client_id=edb6dea4ea1d646cd3bb3f82667f33df&redirect_uri=http://localhost:7891/home&scope=account:write%20trading';
 
-      fetch('http://localhost:7890/api/v1/auth/token', {
-        method: 'Post',
-        headers: { 'content-type': 'application/json' },
-        body: JSON.stringify({ code }),
-      })
-        .then((res) => res.json())
-        .then(({ accessToken }) => {
-          setAccessToken(accessToken);
-        });
-    } else useHistory().push('/home');
+    fetch('http://localhost:7890/api/v1/auth/token', {
+      method: 'Get',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify({ code }),
+    })
+      .then((res) => res.json())
+      .then(console.log(json, accessToken))
+      .then(({ accessToken }) => {
+        setAccessToken(accessToken);
+        //  console.log(acessToken 'acesstoken');
+      });
+    // } else useHistory().push('/home');
   };
   return (
     <AuthContext.Provider value={{ accessToken, redirectToAlpaca }}>
