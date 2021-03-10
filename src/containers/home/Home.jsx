f238e697da2866d66d098948df5eff31c9aafaae
ryanleviathan <ryan.m.carreras@gmail.com>
2021-03-09 17:54:35 -0800

    styling

--- src/containers/home/Home.jsx
+++ src/containers/home/Home.jsx
@@ -1,22 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Header from '../../components/app/header/Header';
 import News from '../../components/app/news/News';
 import UserPagePositions from '../../containers/UserPaperPositions';
-// import Ticker from '../../components/app/ticker/Ticker';
-// import { useParams } from 'react-router-dom';
-function Home({ location }) {
-  // const token = new URLSearchParams(location.search).get('code');
-
-  // const [accessToken, setAccessToken] = useState('');
-  // useEffect(() => {
-  //   setAccessToken(token);
-  // }), [];
-
+import styles from './home.css';
+function Home() {
   return (
     <div>
-      <Header />
-      <News />
-      <UserPagePositions />
+      <Header className={styles.Header}/>
+      <News className={styles.News}/>
+      <UserPagePositions className={styles.Holdings}/>
     </div>
   );
 }
