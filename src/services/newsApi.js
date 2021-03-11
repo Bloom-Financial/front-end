ed5cd3c90736f7848cc5d77dfa86dd1b444ff563
Jumpybuns <ethanpiercepresents@gmail.com>
2021-03-10 18:11:43 -0800

    news api route change

--- src/services/newsApi.js
+++ src/services/newsApi.js
@@ -1,5 +1,5 @@
 export const findArticles = (search) => {
-  return fetch(`http://localhost:7891/news&q=${search}`)
+  return fetch(`https://mysterious-basin-06930.herokuapp.com/news?q=${search}`)
     .then((res) => res.json())
     .then(({ articles }) =>
       articles.map((article) => ({
