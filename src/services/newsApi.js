0e8f9c65458cb58a3b54bd3aa37231f793838c6b
ryanleviathan <ryan.m.carreras@gmail.com>
2021-03-11 10:25:03 -0800

    news api proxy server working

--- src/services/newsApi.js
+++ src/services/newsApi.js
@@ -1,5 +1,6 @@
 export const findArticles = (search) => {
-  return fetch(`http://localhost:7890/news?q=${search}`)
+  return fetch(`https://mysterious-basin-06930.herokuapp.com/news?q=${search}
+  `)
     .then((res) => res.json())
     .then(({ articles }) =>
       articles.map((article) => ({
