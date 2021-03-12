0a7bc5e8de78083e2a92e9c2ac697d013b3358fd
ryanleviathan <ryan.m.carreras@gmail.com>
2021-03-11 20:08:28 -0800

    styling for biopage finished

--- src/containers/bio/BioPage.jsx
+++ src/containers/bio/BioPage.jsx
@@ -7,123 +7,157 @@ import Twitter from './twitterlogo.png';
 import GitHub from './githubicon.png';
 import Ethan from './Ethan.jpg';
 import Ray from './Ray.jpg';
+import styles from './BioPage.css';
 
 export default function BioPage() {
   return (
     <>
-      <h1></h1>
-      <p></p>
-      <img></img>
-      <span>
-        <h1>Sarah Skillen</h1>
-        <p>
-          A software engineer who enjoys solving problems by testing the
-          functions and feels that investing your money should be availble to
-          everyone!
-        </p>
-        <img src={Sarah} height="170px"></img>
-        <a href="https://www.linkedin.com/in/sarah-skillen-7339b61b8/">
-          <img
-            className="linkedinlogo"
-            alt="linkedinlogo"
-            src={Linkedin}
-            height="25px"
-            width="25px"
-          />
-        </a>
-        <a href="https://twitter.com/@SarahSkillen3">
-          <img
-            className="twitterlogo"
-            alt="twitterlogo"
-            src={Twitter}
-            height="25px"
-            width="25px"
-          />
-        </a>
-        <a href="https://github.com/sarah-svg">
-          <img
-            className="githublogo"
-            alt="githublogo"
-            src={GitHub}
-            height="27px"
-            width="27px"
-          />
-        </a>
-      </span>
-
-      <span>
-        <h1>Ryan Carreras</h1>
-        <p>A software engineer who loves finding simple solutions to complex problems, working with a team towards a shared goal, and Stonks. Always love those stonks!
-        </p>
-        <img src={Ryan} height="170px"></img>
-        <a href="https://www.linkedin.com/in/ryancarreras/">
-          <img className="linkedinlogo"
-            alt="linkedinlogo" src={Linkedin} 
-            height="25px" width="25px" />
-        </a>
-        <a href="https://twitter.com/@ryanleviathan">
-          <img className="twitterlogo" alt="twitterlogo"
-            src={Twitter} height="25px" width="25px" />
-        </a>
-        <a href="https://github.com/ryanleviathan">
-          <img className="githublogo" alt="githublogo" 
-            src={GitHub} height="27px" width="27px" />
-        </a>
-      </span>
-
-      <span>
-        <h1>Ethan Pierce</h1>
-        <p>Let us make some money!!</p>
-        <img></img>
-        <img src={Ethan} height="170px"></img>
-        <a href="https://www.linkedin.com/in/ethanpiercepresents/">
-          <img
-            className="linkedinlogo"
-            alt="linkedinlogo"
-            src={Linkedin}
-            height="25px"
-            width="25px"
-          />
-        </a>
-        <a href="https://twitter.com/EthanPierce82">
-          <img
-            className="twitterlogo"
-            alt="twitterlogo"
-            src={Twitter}
-            height="25px"
-            width="25px"
-          />
-        </a>
-        <a href="https://github.com/jumpybuns">
-          <img
-            className="githublogo"
-            alt="githublogo"
-            src={GitHub}
-            height="27px"
-            width="27px"
-          />
-        </a>
-      </span>
-      <span>
-        <h1>Ray Sakultarawattn</h1>
-        <p>A full-stack software developer who loves to get down and dirty with code, and turn it into
-          awesome things that help humans!
-        </p>
-        <img src={Ray} height="170px"></img>
-        <a href="https://www.linkedin.com/in/raysakultarawattn/">
-          <img className="linkedinlogo"
-            alt="linkedinlogo" src={Linkedin} 
-            height="25px" width="25px" />
-        </a>
-        <a href="https://twitter.com/@rsakultarawattn">
-          <img className="twitterlogo" alt="twitterlogo"
-            src={Twitter} height="25px" width="25px" />
-        </a>
-        <a href="https://github.com/rsakultarawattn">
-          <img className="githublogo" alt="githublogo" 
-            src={GitHub} height="27px" width="27px" />
-        </a>
-      </span>
+      <div className={styles.BioContainer}>
+        <span className={styles.Person}>
+          <h1>Sarah Skillen</h1>
+          <img src={Sarah} height="170px"></img>
+          <p>
+            A software engineer who enjoys solving problems by testing the functions and feels that investing your money should be availble to everyone!
+          </p>
+          <div className={styles.links}>
+            <a 
+              className={styles.linkedin}
+              href="https://www.linkedin.com/in/sarah-skillen-7339b61b8/">
+              <img
+                className="linkedinlogo"
+                alt="linkedinlogo"
+                src={Linkedin}
+                height="25px"
+                width="25px"
+              />
+            </a>
+            <a className={styles.twitter} 
+              href="https://twitter.com/@SarahSkillen3">
+              <img
+                className="twitterlogo"
+                alt="twitterlogo"
+                src={Twitter}
+                height="25px"
+                width="25px"
+              />
+            </a>
+            <a className={styles.github}
+              href="https://github.com/sarah-svg">
+              <img
+                className="githublogo"
+                alt="githublogo"
+                src={GitHub}
+                height="27px"
+                width="27px"
+              />
+            </a>
+          </div>
+        </span>
+        <span className={styles.Person}>
+          <h1>Ryan Carreras</h1>
+          <img src={Ryan} height="170px"></img>
+          <p>
+            A software engineer who loves finding simple solutions to complex problems, working with a team towards a shared goal, and Stonks. Always love those stonks!
+          </p>
+          <div className={styles.links}>
+            <a className={styles.linkedin}
+              href="https://www.linkedin.com/in/ryancarreras/">
+              <img 
+                className="linkedinlogo"
+                alt="linkedinlogo" 
+                src={Linkedin} 
+                height="25px" 
+                width="25px" />
+            </a>
+            <a className={styles.twitter}
+              href="https://twitter.com/@ryanleviathan">
+              <img 
+                className="twitterlogo" alt="twitterlogo"
+                src={Twitter} 
+                height="25px" 
+                width="25px" />
+            </a>
+            <a className={styles.github}
+              href="https://github.com/ryanleviathan">
+              <img className="githublogo" alt="githublogo" 
+                src={GitHub} height="27px" width="27px" />
+            </a>
+          </div>
+        </span>
+        <span className={styles.Person}>
+          <h1>Ethan Pierce</h1>
+          <img src={Ethan} height="170px"></img>
+          <p>
+            Let us make some money!!
+          </p>
+          <div className={styles.links}>
+            <a className={styles.linkedin} 
+              href="https://www.linkedin.com/in/ethanpiercepresents/">
+              <img
+                className="linkedinlogo"
+                alt="linkedinlogo"
+                src={Linkedin}
+                height="25px"
+                width="25px"
+              />
+            </a>
+            <a className={styles.twitter}
+              href="https://twitter.com/EthanPierce82">
+              <img
+                className="twitterlogo"
+                alt="twitterlogo"
+                src={Twitter}
+                height="25px"
+                width="25px"
+              />
+            </a>
+            <a className={styles.github}
+              href="https://github.com/jumpybuns">
+              <img
+                className="githublogo"
+                alt="githublogo"
+                src={GitHub}
+                height="27px"
+                width="27px"
+              />
+            </a>
+          </div>
+        </span>
+        <span className={styles.Person}>
+          <h1>Ray Sakultarawattn</h1>
+          <img src={Ray} height="170px"></img>
+          <p>
+            A full-stack software developer who loves to get down and dirty with code, and turn it into awesome things that help humans!
+          </p>
+          <div className={styles.links}>
+            <a className={styles.linkedin} 
+              href="https://www.linkedin.com/in/raysakultarawattn/">
+              <img 
+                className="linkedinlogo"
+                alt="linkedinlogo" 
+                src={Linkedin} 
+                height="25px" 
+                width="25px" />
+            </a>
+            <a className={styles.twitter}
+              href="https://twitter.com/@rsakultarawattn">
+              <img 
+                className="twitterlogo" alt="twitterlogo"
+                src={Twitter} 
+                height="25px" 
+                width="25px" />
+            </a>
+            <a className={styles.github}
+              href="https://github.com/rsakultarawattn">
+              <img 
+                className="githublogo" alt="githublogo" 
+                src={GitHub} 
+                height="27px" 
+                width="27px" />
+            </a>
+          </div>
+        </span>
+      </div>
     </>
   );
 }
