b5867f90942dd74235c6bde0d2fff09afd13469a
ryanleviathan <ryan.m.carreras@gmail.com>
2021-03-11 21:41:33 -0800

    added tickers to header, mostly styled

--- src/containers/home/Home.jsx
+++ src/containers/home/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Header from '../../components/app/header/Header';
 import News from '../../components/app/news/News';
 import UserPagePositions from '../../containers/UserPaperPositions';
-import TickerPage from '../TickerPage';
 import styles from './home.css';
 import TickerWidget from '../../components/TickerWidget';
 
@@ -11,7 +10,6 @@ function Home() {
     <div>
       <Header className={styles.Header} />
       <TickerWidget />
-      <TickerPage className={styles.Ticker} />
       <UserPagePositions className={styles.Holdings} />
       <News className={styles.News} />
     </div>
